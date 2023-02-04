62c2a998fc90f18fe19e7448b37f20d33d2b2b4f
Vaibhav <vaibhavghawale007@gmail.com>
2023-02-05 00:44:52 +0530

    bugs remove from service registry eureka dependancy corrected

--- HotelMicroservice/src/main/java/com/hotel/HotelMicroserviceApplication.java
+++ HotelMicroservice/src/main/java/com/hotel/HotelMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelMicroserviceApplication {
 
 	public static void main(String[] args) {
--- UserMicroservice/src/main/java/com/user/UserMicroserviceApplication.java
+++ UserMicroservice/src/main/java/com/user/UserMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
