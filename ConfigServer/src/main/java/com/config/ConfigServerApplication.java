a4e042fd4fba1f778adeca4022fe759e790bbc85
Vaibhav <vaibhavghawale007@gmail.com>
2023-02-28 00:39:07 +0530

    Configuration serveer created- Git configuration pending

---
+++ ConfigServer/src/main/java/com/config/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
